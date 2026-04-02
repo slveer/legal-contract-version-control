41c2f23cda9641f1dbebfef17938cfb17036fc2c
Daniel <danielaphillion@gmail.com>
2026-04-02 15:33:49 -0400

    feat: parse document.xml to get all <w:t>

--- parse.py
+++ parse.py
@@ -1,1 +1,12 @@
 # This file will contain the parsing logic to get all <w:t> and <w:tbl> elements from a Word document
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('document.xml')
+root = tree.getroot()
+list = []
+
+for item in root.iter("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}t"):
+    if item.text:
+        list.append(item.text)
+
+print("".join(list))
