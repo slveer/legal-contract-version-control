58678e0d966b2436eb073dcb974d3bf91720e294
Daniel <danielaphillion@gmail.com>
2026-04-03 19:00:15 -0400

    refactor: optimize .docx checking logic

--- docx_to_text.py
+++ docx_to_text.py
@@ -7,24 +7,20 @@ import string
 import os
 
 path = sys.argv[2] if len(sys.argv) > 1 else None
-striped_path = path.strip(".docx")
 
-if '.docx' in path:
-    text = docx2txt.process(path)
-
-elif Path(path).is_file():
-    text = docx2txt.process(path)
-
-else:
+if path and path.endswith(".docx") and Path(path).is_file():
+    docx_to_txt = docx2txt.process(path)
+else: 
     print("Invalid file path, make sure the file exists and is a .docx file")
     sys.exit(1)
 
-subprocess.run(["unzip", f"{path}", "-d", striped_path])
+
+subprocess.run(["unzip", f"{path}", "-d", path.strip(".docx")])
 
 file_hash = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
 
-with open(f"{striped_path}/{file_hash}.txt", "w") as f:
-    f.write(text)
+with open(f"{path.strip('.docx')}/{file_hash}.txt", "w") as f:
+    f.write(docx_to_txt)
 
 
     
