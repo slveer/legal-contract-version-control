fc923055d71df920f228aa86be8f314cfe875b11
Daniel <danielaphillion@gmail.com>
2026-04-04 08:14:48 -0400

    feat: add .docx check

--- docx_to_text.py
+++ docx_to_text.py
@@ -7,11 +7,14 @@ import string
 import os
 
 path = sys.argv[2] if len(sys.argv) > 1 else None
-
-if Path(path).is_file():
+if '.docx' in path:
     text = docx2txt.process(path)
+
+elif Path(path).is_file():
+    text = docx2txt.process(path)
+
 else:
-    print("Invalid file path")
+    print("Invalid file path, make sure the file exists and is a .docx file")
     sys.exit(1)
 
 subprocess.run(["unzip", f"{path}", "-d", path.strip(".docx")])
