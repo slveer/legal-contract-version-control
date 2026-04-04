1dd9503ba72a625cda39a42e6baf6033088acf25
Daniel <danielaphillion@gmail.com>
2026-04-04 07:22:14 -0400

    feat: add comments

--- docx_to_text.py
+++ docx_to_text.py
@@ -5,24 +5,34 @@ import docx2txt
 import hashlib
 from datetime import datetime
 
+# Get user inputted path argument
 path = sys.argv[2] if len(sys.argv) > 1 else None
+
+# Strip .docx extension from the file name to create a directory
 directory_path = f"{path.strip('.docx')}"
 
+# Check if the directory already contains an SCCS initialization
 if Path(f"{directory_path}/.sccs").is_dir():
     print("This file has already been initialized with SCCS")
     sys.exit(1)
 
+# Check if the path ends with .docx and exists
 elif path and path.endswith(".docx") and Path(path).is_file():
     docx_to_txt = docx2txt.process(path)
+    
+# if not, exit  
 else: 
     print("Invalid file path, make sure the file exists and is a .docx file")
     sys.exit(1)
 
+# Get user inputted name and email
 name = input("Enter your name: ")
 email = input("Enter your email: ")
 
+# Generate a SHA-256 hash for the initial commit
 sha_hash = hashlib.sha256(f'{datetime.now().isoformat()}/initial_version'.encode()).hexdigest()
 
+# Create needed directories
 subprocess.run(["mkdir", "-p", directory_path])
 subprocess.run(["mv", path, directory_path])
 subprocess.run(["mkdir", "-p", f"{directory_path}/.sccs"])
@@ -31,6 +41,7 @@ subprocess.run(["mkdir", "-p", f"{directory_path}/.sccs/history"])
 subprocess.run(["mkdir", "-p", f"{directory_path}/.sccs/commit_messages"])
 subprocess.run(["mkdir", "-p", f"{directory_path}/.sccs/config"])
 
+# Add info to the directories, JSON
 with open(f"{directory_path}/.sccs/commits/{sha_hash}.txt", "w") as f:
     f.write(docx_to_txt)
 
