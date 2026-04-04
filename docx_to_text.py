db6e8ffa1120c8d7123ebb137cf24de766b088f4
Daniel <danielaphillion@gmail.com>
2026-04-04 09:49:40 -0400

    feat: unzip .docx to folder of same name as .docx, add .txt file to folder

--- docx_to_text.py
+++ docx_to_text.py
@@ -1,4 +1,4 @@
-from asyncio import subprocess
+import subprocess
 from pathlib import Path
 import random
 import sys
@@ -7,6 +7,8 @@ import string
 import os
 
 path = sys.argv[2] if len(sys.argv) > 1 else None
+striped_path = path.strip(".docx")
+
 if '.docx' in path:
     text = docx2txt.process(path)
 
@@ -17,11 +19,12 @@ else:
     print("Invalid file path, make sure the file exists and is a .docx file")
     sys.exit(1)
 
-subprocess.run(["unzip", f"{path}", "-d", path.strip(".docx")])
+subprocess.run(["unzip", f"{path}", "-d", striped_path])
 
 file_hash = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
 
-with open(f"{file_hash}.txt", "w") as f:
+with open(f"{striped_path}/{file_hash}.txt", "w") as f:
     f.write(text)
 
 
+    
