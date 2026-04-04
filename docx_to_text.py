754927a1ab1543ee3bf879a55a920ee4e059431d
Daniel <danielaphillion@gmail.com>
2026-04-03 20:31:05 -0400

    feat: make .txt filename a hex digest

--- docx_to_text.py
+++ docx_to_text.py
@@ -2,6 +2,8 @@ import subprocess
 from pathlib import Path
 import sys
 import docx2txt 
+import hashlib
+from datetime import datetime
 
 path = sys.argv[2] if len(sys.argv) > 1 else None
 
@@ -11,10 +13,12 @@ else:
     print("Invalid file path, make sure the file exists and is a .docx file")
     sys.exit(1)
 
+sha_hash = hashlib.sha256(f'{datetime.now().isoformat()}/initial_version'.encode()).hexdigest()
+
 subprocess.run(["mkdir", "-p", path.strip(".docx")])
 subprocess.run(["mv", path, path.strip(".docx")])
 subprocess.run(["mkdir", "-p", f"{path.strip('.docx')}/.sccs"])
 
-with open(f"{path.strip('.docx')}/.sccs/output.txt", "w") as f:
+with open(f"{path.strip('.docx')}/.sccs/{sha_hash}.txt", "w") as f:
     f.write(docx_to_txt)
 
