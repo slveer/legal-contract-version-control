6d752623d6049de1eb6c9ee1f44e2fb68221a24e
Daniel <danielaphillion@gmail.com>
2026-04-04 17:12:41 -0400

    feat: sccs cmds can be used anywhere, not just in repo

--- sccs.bat
+++ sccs.bat
@@ -4,24 +4,17 @@ setlocal enabledelayedexpansion
 set "command=%1"
 
 if "%command%"=="init" (
-    python init.py %*
+    set "script_directory=%~dp0"
+    python "%script_directory%init.py" %*
     exit /b !errorlevel!
 )
 
 if "%command%"=="commit" (
-    python commit.py %*
+    set "script_directory=%~dp0"
+    python "%script_directory%commit.py" %*
     exit /b !errorlevel!
 )
 
 echo Unknown command: %command%
 echo Invalid command. Please use either "init" or "commit", along with required arguments
 exit /b 1
-
-
-@REM Instructions to set up sccs.bat in CLI on Windows:
-
-@REM Search "environment variables" in the Start menu
-@REM Click "Edit the system environment variables"
-@REM Click "Environment Variables"
-@REM Under "User variables", find Path and click Edit
-@REM Add the folder where sccs.bat lives, e.g. C:\Users\YOUR_USERNAME\bin
