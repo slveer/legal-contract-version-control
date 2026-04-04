ced65e56d0b665556af4cb13e515abe7dffda955
Daniel <danielaphillion@gmail.com>
2026-04-04 07:35:50 -0400

    feat: create sccs.bat with detailed instruction for how to set up

--- sccs.bat
+++ sccs.bat
@@ -1,1 +1,17 @@
-@REM TODO: Make windows functionality for sccs CLI
+#!/usr/bin/env python3 
+
+import sys
+import subprocess
+
+command = sys.argv[1] if len(sys.argv) > 1 else None
+
+if command == "init":
+    subprocess.run(["python3", "docx_to_text.py"] + sys.argv[1:]) 
+
+@REM # Instructions to set up sccs.bat in CLI on Windows
+
+@REM Search "environment variables" in the Start menu
+@REM Click "Edit the system environment variables"
+@REM Click "Environment Variables"
+@REM Under "User variables", find Path and click Edit
+@REM Add the folder where sccs.bat lives, e.g. C:\Users\YOUR_USERNAME\bin
