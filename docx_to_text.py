78c07ba3085dacc768fbada154b3326a48025d94
Daniel <danielaphillion@gmail.com>
2026-04-04 08:11:31 -0400

    feat: unzip file to folder

--- docx_to_text.py
+++ docx_to_text.py
@@ -1,8 +1,10 @@
+from asyncio import subprocess
 from pathlib import Path
 import random
 import sys
 import docx2txt 
 import string
+import os
 
 path = sys.argv[2] if len(sys.argv) > 1 else None
 
@@ -11,8 +13,12 @@ if Path(path).is_file():
 else:
     print("Invalid file path")
     sys.exit(1)
-    
+
+subprocess.run(["unzip", f"{path}", "-d", path.strip(".docx")])
+
 file_hash = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
 
 with open(f"{file_hash}.txt", "w") as f:
     f.write(text)
+
+
