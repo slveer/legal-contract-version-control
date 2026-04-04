19bf661c07417ddf8531d85e4e1499a8405a3e2d
Daniel <danielaphillion@gmail.com>
2026-04-04 07:19:50 -0400

    feat: sccs init cmd now takes file path arg, and uses that for docx2txt conversion

--- docx_to_text.py
+++ docx_to_text.py
@@ -1,11 +1,18 @@
+from pathlib import Path
 import random
+import sys
 import docx2txt 
 import string
 
-text = docx2txt.process("/Users/danielphillion/Library/Containers/com.apple.mail/Data/Library/Mail Downloads/493F5694-AD2A-4C27-AEBB-6A65007EB42E/Doc1.docx")
+path = sys.argv[2] if len(sys.argv) > 1 else None
 
+if Path(path).is_file():
+    text = docx2txt.process(path)
+else:
+    print("Invalid file path")
+    sys.exit(1)
+    
 file_hash = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
 
 with open(f"{file_hash}.txt", "w") as f:
     f.write(text)
-
