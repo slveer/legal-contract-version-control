a80a7bab3064095fbe2e31bdf1a228cbe44ad1d9
Daniel <danielaphillion@gmail.com>
2026-04-04 16:22:49 -0400

    feat: give directory path the correct path

--- commit.py
+++ commit.py
@@ -13,10 +13,16 @@ path = sys.argv[2] if len(sys.argv) > 2 else None
 # Strip .docx extension from the file name to create a directory
 # Because sccs init moves the file into the directory, update the path to point to the moved file
 if path: 
-    if not Path(path).is_file():
-        path = os.path.join(directory_path, os.path.basename(path))
-        
-    directory_path = Path(path).parent
+    
+    directory_path = Path(path).with_suffix(" ")
+
+    # if directory basename = parent directory name (eg: user/file/file), use parent directory (eg: user/file).
+    if directory_path.name == directory_path.parent.name:
+        directory_path = directory_path.parent
+
+    # Set path correctly, whether it was correct in the first place or not
+    path = os.path.join(directory_path, os.path.basename(path))
+
     if not Path(path).is_file():
         print("File not found. Please provide a valid file path.")
         sys.exit(1)
