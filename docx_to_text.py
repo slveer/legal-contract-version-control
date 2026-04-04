66b588cc9c5a5d4825fd0c609857c8543c115495
Daniel <danielaphillion@gmail.com>
2026-04-04 07:06:10 -0400

    feat: check if file has already been initialized

--- docx_to_text.py
+++ docx_to_text.py
@@ -7,7 +7,13 @@ from datetime import datetime
 
 path = sys.argv[2] if len(sys.argv) > 1 else None
 
-if path and path.endswith(".docx") and Path(path).is_file():
+
+
+if Path("/Users/danielphillion/Documents/Cher-Père-Noël/.sccs").is_dir(): #f"{path.strip('.docx')}/.sccs"
+    print("This file has already been initialized with SCCS")
+    sys.exit(1)
+
+elif path and path.endswith(".docx") and Path(path).is_file():
     docx_to_txt = docx2txt.process(path)
 else: 
     print("Invalid file path, make sure the file exists and is a .docx file")
