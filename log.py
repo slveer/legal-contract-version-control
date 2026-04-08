7c72796028cf4a9908c018c56f577ebb2fc5af40
Daniel <252123108+slveer@users.noreply.github.com>
2026-04-08 12:50:07 +0000

    feat: print all log data from commit_log.json

--- log.py
+++ log.py
@@ -31,4 +31,22 @@ if not Path(os.path.join(directory_path, ".sccs")).is_dir():
 elif not path and Path(path).suffix.lower() == ".docx" and Path(path).is_file():
     print("Invalid file path, make sure the file exists and is a .docx file")
     sys.exit(1)
-    
+
+# Get JSON log data
+log_path = os.path.join(directory_path, ".sccs", "history", "commit_log.json")
+if not Path(log_path).is_file():
+    print("Log file not found. Please make sure the file has been initialized with SCCS.")
+    sys.exit(1)
+
+with open(log_path, "r", encoding="utf-8", newline="\n") as log_file:
+    log_data = json.load(log_file)
+# Print log data
+for entry in log_data:
+    print(
+        "------------------------------\n"
+        f"Commit ID: {entry}\n"
+        f"Author: {log_data[entry]['author']}\n"
+        f"Date: {log_data[entry]['timestamp']}\n"
+        f"Message: {log_data[entry]['message']}\n"
+        "-----------------------------"
+    )
