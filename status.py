dfe94b1f9b38a1633ab4a95bcb4d3fe033b28423
Daniel <danielaphillion@gmail.com>
2026-04-14 18:38:18 -0400

    feat: add type and exists checks for sccs status

---
+++ status.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import sys
+import docx2txt 
+import hashlib
+from datetime import datetime
+
+
+# Get user inputted path argument
+path = sys.argv[2] if len(sys.argv) > 2 else None
+
+# Strip .docx extension from the file name to create a directory
+# Because sccs init moves the file into the directory, update the path to point to the moved file
+if path: 
+
+    directory_path = Path(path).with_suffix("")
+
+    # if directory basename = parent directory name (eg: user/file/file), use parent directory (eg: user/file).
+    if directory_path.name == directory_path.parent.name:
+        directory_path = directory_path.parent
+
+    # Set path correctly, whether it was correct in the first place or not
+    path = os.path.join(directory_path, os.path.basename(path))
+
+    if not Path(path).is_file():
+        print("File not found. Please provide a valid file path.")
+        sys.exit(1)
+else:
+    print("No file path provided")
+    sys.exit(1)
+
+# Check if the directory contains an SCCS initialization
+if not Path(os.path.join(directory_path, ".sccs")).is_dir():
+    print("This file has not been initialized with SCCS.")
+    print("Please run 'sccs init <file_path>' to initialize SCCS for this file.")
+    sys.exit(1)
+
+elif path and Path(path).suffix.lower() == ".docx" and Path(path).is_file():
+    try:
+        with open(path, "rb") as f:
+            hashed_file = hashlib.sha256(f.read()).hexdigest()
+    except Exception as e:
+        print(f"Error processing .docx file: {e}")
+        sys.exit(1)
+
+# if not, exit  
+else: 
+    print("Invalid file path, make sure the file exists and is a .docx file")
+    sys.exit(1)
+
