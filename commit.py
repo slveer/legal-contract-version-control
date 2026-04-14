8e27f45b0345a80f1733ff1e0d263b69f5ab81a4
Daniel <danielaphillion@gmail.com>
2026-04-14 06:38:03 -0400

    feat: get path, directory path from the CWD

--- commit.py
+++ commit.py
@@ -7,52 +7,20 @@ import hashlib
 from datetime import datetime
 import json
 
+directory_path = os.getcwd()
 
-# Get user inputted path argument
-path = sys.argv[2] if len(sys.argv) > 2 else None
+path = os.path.join(directory_path, f"{os.path.basename(directory_path)}.docx")
 
-# Strip .docx extension from the file name to create a directory
-# Because sccs init moves the file into the directory, update the path to point to the moved file
-if path: 
-
-    directory_path = Path(path).with_suffix("")
-
-    # if directory basename = parent directory name (eg: user/file/file), use parent directory (eg: user/file).
-    if directory_path.name == directory_path.parent.name:
-        directory_path = directory_path.parent
-
-    # Set path correctly, whether it was correct in the first place or not
-    path = os.path.join(directory_path, os.path.basename(path))
-
-    if not Path(path).is_file():
-        print("File not found. Please provide a valid file path.")
-        sys.exit(1)
-else:
-    print("No file path provided")
-    sys.exit(1)
-
-# Check if the directory contains an SCCS initialization
-if not Path(os.path.join(directory_path, ".sccs")).is_dir():
-    print("This file has not been initialized with SCCS.")
-    print("Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-elif path and Path(path).suffix.lower() == ".docx" and Path(path).is_file():
-    try: 
-        commit = docx2txt.process(path)
-        with open(path, "rb") as f:
-            hashed_file = hashlib.sha256(f.read()).hexdigest()
-            hasher = hashlib.sha256()
-            for chunk in iter(lambda: f.read(65536), b""):
-                hasher.update(chunk)
-            hashed_file = hasher.hexdigest()
-    except Exception as e:
-        print(f"Error processing .docx file: {e}")
-        sys.exit(1)
-
-# if not, exit  
-else: 
-    print("Invalid file path, make sure the file exists and is a .docx file")
+try: 
+    commit = docx2txt.process(path)
+    with open(path, "rb") as f:
+        hashed_file = hashlib.sha256(f.read()).hexdigest()
+        hasher = hashlib.sha256()
+        for chunk in iter(lambda: f.read(65536), b""):
+            hasher.update(chunk)
+        hashed_file = hasher.hexdigest()
+except Exception as e:
+    print(f"Error processing .docx file: {e}")
     sys.exit(1)
 
 # Get name and email entered on init
