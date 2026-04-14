4f386740936ec559c68a214408472d93adb131f6
Daniel <danielaphillion@gmail.com>
2026-04-14 14:32:06 -0400

    fix: delete commented code

--- open.py
+++ open.py
@@ -3,39 +3,6 @@ import shutil
 import sys
 from pathlib import Path
 
-# # Get user inputted path argument
-# path = sys.argv[2] if len(sys.argv) > 2 else None
-
-# # Check if the path is provided
-# if path: 
-
-#     directory_path = Path(path).with_suffix("")
-
-#     # if directory basename = parent directory name (eg: user/file/file), use parent directory (eg: user/file).
-#     if directory_path.name == directory_path.parent.name:
-#         directory_path = directory_path.parent
-
-#     # Set path correctly, whether it was correct in the first place or not
-#     path = os.path.join(directory_path, os.path.basename(path))
-
-#     if not Path(path).is_file():
-#         print("Invalid file path, make sure the file exists and is a .docx file")
-#         sys.exit(1)
-# else:
-#     print("No file path provided")
-#     sys.exit(1)
-
-# # Check if the directory contains an SCCS initialization
-# if not Path(os.path.join(directory_path, ".sccs")).is_dir():
-#     print("This file has not been initialized with SCCS.")
-#     print("Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-#     sys.exit(1)
-    
-# # Check if the path ends with .docx and exists
-# if not Path(path).is_file() or Path(path).suffix.lower() != ".docx":
-#     print("Invalid file path, make sure the file exists and is a .docx file")
-#     sys.exit(1)
-
 directory_path = os.getcwd()
 
 path = os.path.join(directory_path, f"{os.path.basename(directory_path)}.docx")
