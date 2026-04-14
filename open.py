9fe69eb61b0df3c666d80f821fad00f3cf6d1190
Daniel <danielaphillion@gmail.com>
2026-04-13 20:09:11 -0400

    refactor: import layout check module

--- open.py
+++ open.py
@@ -3,64 +3,13 @@ import shutil
 import sys
 from pathlib import Path
 
-directory_path = os.getcwd()
+from sccs_layout_check import directory_path
 
-path = os.path.join(directory_path, f"{os.path.basename(directory_path)}.docx")
+from sccs_layout_check import path
 
-sccs_dir = os.path.join(directory_path, ".sccs")
+from sccs_layout_check import check_sccs
 
-if not Path(sccs_dir).is_dir():
-    print("This file has not been initialized with SCCS.")
-    print("Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_file_hash")).is_dir():
-    print("Commit file hash directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_file_hash", "commit_file_hash.json")).is_file():
-    print("Commit file hash JSON not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_messages")).is_dir():
-    print("Commit messages directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_messages", "commit_messages.json")).is_file():
-    print("Commit messages JSON not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commits")).is_dir():
-    print("Commits directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commits", "txt-commits")).is_dir():
-    print("Text commits directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commits", "docx-commits")).is_dir():
-    print("Docx commits directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "config")).is_dir():
-    print("Config directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "config", "config.json")).is_file():
-    print("Config file not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "history")).is_dir():
-    print("History directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "history", "commit_history.json")).is_file():
-    print("History file not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(path).is_file():
-    print("Docx file not found. Re-initialize SCCS for this file with 'sccs init <file_path>'")
-    sys.exit(1)
+check_sccs()
 
 # Get user inputted commit path
 commit_path = input("Enter the path to the commit file (.docx): ")
--- status.py
+++ status.py
@@ -4,64 +4,13 @@ import sys
 import hashlib
 import json
 
-directory_path = os.getcwd()
+from sccs_layout_check import directory_path
 
-path = os.path.join(directory_path, f"{os.path.basename(directory_path)}.docx")
+from sccs_layout_check import path
 
-sccs_dir = os.path.join(directory_path, ".sccs")
+from sccs_layout_check import check_sccs
 
-if not Path(sccs_dir).is_dir():
-    print("This file has not been initialized with SCCS.")
-    print("Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_file_hash")).is_dir():
-    print("Commit file hash directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_file_hash", "commit_file_hash.json")).is_file():
-    print("Commit file hash JSON not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_messages")).is_dir():
-    print("Commit messages directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commit_messages", "commit_messages.json")).is_file():
-    print("Commit messages JSON not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commits")).is_dir():
-    print("Commits directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commits", "txt-commits")).is_dir():
-    print("Text commits directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "commits", "docx-commits")).is_dir():
-    print("Docx commits directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "config")).is_dir():
-    print("Config directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "config", "config.json")).is_file():
-    print("Config file not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "history")).is_dir():
-    print("History directory not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(os.path.join(sccs_dir, "history", "commit_history.json")).is_file():
-    print("History file not found. Please run 'sccs init <file_path>' to initialize SCCS for this file.")
-    sys.exit(1)
-
-if not Path(path).is_file():
-    print("Docx file not found. Re-initialize SCCS for this file with 'sccs init <file_path>'")
-    sys.exit(1)
+check_sccs()
 
 try:
     with open(path, "rb") as f:
