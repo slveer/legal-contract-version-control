2690037db428dde25b3798b1f03288f48bee3171
Daniel <danielaphillion@gmail.com>
2026-04-21 17:13:37 -0400

    fix: rename conflicting functions

--- diff.py
+++ diff.py
@@ -82,14 +82,14 @@ if not Path(docx_current_version).is_file():
 
 def strip_tags(html: str) -> str:
     counter = 0
-    def replace_tag(match):
+    def add_number_attribute(match):
         nonlocal counter
         tag = match.group(1)
         inner = re.sub(r'<[^>]+>', '', match.group(2))
         result = f'<{tag} number="{counter}">{inner}</{tag}>'
         counter += 1
         return result
-    return re.sub(r"<(h1|h2|h3|h4|h5|h6|p|li|blockquote|a)>(.*?)</\1>", replace_tag, html, flags=re.DOTALL)
+    return re.sub(r"<(h1|h2|h3|h4|h5|h6|p|li|blockquote|a)>(.*?)</\1>", add_number_attribute, html, flags=re.DOTALL)
 
 def html_el_to_list(html: str) -> list:
     text = re.findall(r'<(?:h1|h2|h3|h4|h5|h6|p|li|blockquote|a) number="\d+">(.*?)</(?:h1|h2|h3|h4|h5|h6|p|li|blockquote|a)>', html)
