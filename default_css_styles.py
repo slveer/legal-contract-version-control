4a43ea06237ff3f2b56f8ee0436fe114c26d2864
Daniel <danielaphillion@gmail.com>
2026-04-24 14:46:38 -0400

    fix: diffed el with take up required with and start on new line

--- default_css_styles.py
+++ default_css_styles.py
@@ -1,7 +1,22 @@
 styles = """<style>
-* {font-family: Arial, Helvetica, sans-serif;}
-.inserted {background-color: #d4fcbc;
-display: inline;}
-.deleted {background-color: #fbb6c2;
-display: inline;}
+* {
+font-family: Arial, Helvetica, sans-serif;}
+
+.inserted {
+
+background-color: #d4fcbc;
+
+display: block;
+
+width: fit-content;
+}
+
+.deleted {
+
+background-color: #fbb6c2;
+
+display: block;
+
+width: fit-content;
+}
 </style>"""
