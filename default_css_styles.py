566263d525aea931ceb9d4448133f3484826fa09
Daniel <danielaphillion@gmail.com>
2026-04-24 14:41:11 -0400

    feat: add display inline to default styles for diff highlighting

--- default_css_styles.py
+++ default_css_styles.py
@@ -1,5 +1,7 @@
 styles = """<style>
 * {font-family: Arial, Helvetica, sans-serif;}
-.inserted {background-color: #d4fcbc;}
-.deleted {background-color: #fbb6c2;}
+.inserted {background-color: #d4fcbc;
+display: inline;}
+.deleted {background-color: #fbb6c2;
+display: inline;}
 </style>"""
