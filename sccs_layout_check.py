612f162920103f8d297c5e83b74910ceff3cd451
Daniel <252123108+slveer@users.noreply.github.com>
2026-04-29 16:48:45 -0400

    Revert "Feat/switch branches command"

--- sccs_layout_check.py
+++ sccs_layout_check.py
@@ -1,7 +1,6 @@
 import json
 import os
 from pathlib import Path
-import re
 import sys
 from default_css_styles import styles
 
@@ -10,9 +9,6 @@ directory_path = os.getcwd()
 path = os.path.join(directory_path, f"{os.path.basename(directory_path)}.docx")
 
 sccs_dir = os.path.join(directory_path, ".sccs")
-
-def sanitize_dirname(name):
-    return re.sub(r'[\\/:*?"<>|]', '-', name).strip('. ')
 
 def check_sccs():
 
