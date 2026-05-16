d795495cfdb541bb973e43027f082eb9e6f4b6b8
Daniel <danielaphillion@gmail.com>
2026-05-16 06:53:42 -0400

    feat: extract and mount the uploaded file

--- API/main.py
+++ API/main.py
@@ -7,15 +7,21 @@ from fastapi.responses import StreamingResponse
 from pathlib import Path
 import os
 
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
+
+app.mount("/extracted", StaticFiles(directory="API/extracted"), name="extracted")
 
 
 @app.get("/")
 async def root():
     return {"Boo!"}
 
+
 @app.post("/publish")
 async def publish(file: UploadFile = File(...)):
-    with open(f"{file.filename}", "wb") as f:
-        shutil.copyfileobj(file.file, f)
-    return {"filename": file.filename, "path": os.path.abspath(f"{file.filename}")}
+    with zipfile.ZipFile(file.file, "r") as f:
+        f.extractall(f"API/extracted/{file.filename}")
+    return {}
+
