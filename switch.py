fe6d0fc933804ffcee158b853b8dd0ce2b86e0b1
Daniel <danielaphillion@gmail.com>
2026-05-19 09:26:22 -0400

    feat: refactor status logic

--- switch.py
+++ switch.py
@@ -8,6 +8,55 @@ check_sccs()
 
 branch_to_switch = sys.argv[2] if len(sys.argv) > 2 else None
 
+try:
+    with open(os.path.join(directory_path, ".sccs", "current_branch", "current_branch.json"), "r", encoding="utf-8", newline="\n") as f:
+        try:
+            branch = json.load(f).get("current_branch")
+        except Exception as e:
+            print(f"Error reading current branch information: {e}")
+            sys.exit(1)
+except Exception as e:
+    print(f"Error accessing current branch information: {e}")
+    sys.exit(1)
+
+try: 
+    with open(os.path.join(directory_path, ".sccs", "branches", branch, "history", "commit_history.json"), "r", encoding="utf-8", newline="\n") as f:
+        try:
+            latest_commit = json.load(f).get("history")["latest_commit"]
+        except Exception as e:
+            print(f"Error reading commit history for branch '{branch}': {e}")
+            sys.exit(1)
+except Exception as e:
+    print(f"Error accessing commit history for branch '{branch}': {e}")
+    sys.exit(1)
+
+try:
+    with open(os.path.join(directory_path, ".sccs", "branches", branch, "commit_file_hash", "commit_file_hash.json"), "r", encoding="utf-8", newline="\n") as f:
+        try:
+            latest_commit_file_hash = json.load(f).get(latest_commit)
+        except Exception as e:
+            print(f"Error reading commit file hash for branch '{branch}': {e}")
+            sys.exit(1)
+except Exception as e:
+    print(f"Error accessing commit file hash for branch '{branch}': {e}")
+    sys.exit(1)
+
+try:
+    with open(path, "rb") as f:
+        hasher = hashlib.sha256()
+        for chunk in iter(lambda: f.read(65536), b""):
+            hasher.update(chunk)
+        hashed_file = hasher.hexdigest()
+except Exception as e:
+    print(f"Error processing .docx file: {e}")
+    sys.exit(1)
+
+if not hashed_file == latest_commit_file_hash:
+    print(hashed_file)
+    print(latest_commit_file_hash)
+    print(f"Error: The current file has uncommitted changes on the current branch '{branch}'. Please commit your changes before switching branches.." )
+    sys.exit(1)
+
 if branch_to_switch:
     branch_to_switch = sanitize_dirname(branch_to_switch)
 
@@ -67,7 +116,7 @@ try:
     shutil.copy2(os.path.join(directory_path, ".sccs", "objects", "docx", f"{latest_commit_on_branch_to_switch}.docx"), os.path.join(directory_path, f"{os.path.basename(directory_path)}.docx"))
 except Exception as e:
     print(f"Error switching to branch '{branch_to_switch}': {e}")
-    
+
     sys.exit(1)
 
 print(f"Successfully switched to branch '{branch_to_switch}'.")
