e41f50dd32c2d978ba75d85a7f0d7f0bbb72f14a
Daniel <danielaphillion@gmail.com>
2026-05-22 14:47:41 -0400

    fix: move check_sccs

--- log.py
+++ log.py
@@ -9,12 +9,14 @@ from sccs_layout_check import check_sccs
 
 CURRENT_BRANCH_PATH = os.path.join(directory_path, ".sccs", "current_branch", "current_branch.json")
 
+check_sccs()
+
 def get_current_branch():
     with open(CURRENT_BRANCH_PATH, "r", encoding="utf-8", newline="\n") as current_branch_file:
             current_branch = json.load(current_branch_file).get("current_branch")
     return current_branch
 
-check_sccs()
+
 
 # Get JSON log data
 log_path = os.path.join(directory_path, ".sccs", "branches", current_branch, "history", "commit_history.json")
