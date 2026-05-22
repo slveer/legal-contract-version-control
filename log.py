d4110526991a67dcd35f13730d3205fd1f6b5249
Daniel <danielaphillion@gmail.com>
2026-05-22 14:50:12 -0400

    feat: create func to get log data

--- log.py
+++ log.py
@@ -16,23 +16,25 @@ def get_current_branch():
             current_branch = json.load(current_branch_file).get("current_branch")
     return current_branch
 
+def get_log_data():
+    # Get JSON log data
+    log_path = os.path.join(directory_path, ".sccs", "branches", get_current_branch, "history", "commit_history.json")
+    if not Path(log_path).is_file():
+        print("Log file not found. Please make sure the file has been initialized with SCCS.")
+        sys.exit(1)
+
+    with open(log_path, "r", encoding="utf-8", newline="\n") as log_file:
+        log_data = json.load(log_file)
+    return log_data
 
 
-# Get JSON log data
-log_path = os.path.join(directory_path, ".sccs", "branches", current_branch, "history", "commit_history.json")
-if not Path(log_path).is_file():
-    print("Log file not found. Please make sure the file has been initialized with SCCS.")
-    sys.exit(1)
-
-with open(log_path, "r", encoding="utf-8", newline="\n") as log_file:
-    log_data = json.load(log_file)
 # Print log data
-for entry in log_data["log"]:
+for entry in get_log_data()["log"]:
     print(
         "------------------------------\n"
         f"Commit File: {entry}\n"
-        f"Author: {log_data['log'][entry]['author']}\n"
-        f"Date: {log_data['log'][entry]['timestamp']}\n"
-        f"Message: {log_data['log'][entry]['message']}\n"
+        f"Author: {get_log_data()['log'][entry]['author']}\n"
+        f"Date: {get_log_data()['log'][entry]['timestamp']}\n"
+        f"Message: {get_log_data()['log'][entry]['message']}\n"
         "------------------------------"
     )
