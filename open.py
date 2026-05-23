6b8bc1356deb98b56ba24714674237fd5b12465b
Daniel <danielaphillion@gmail.com>
2026-05-23 11:45:37 -0400

    feat: func to validate the commit path

--- open.py
+++ open.py
@@ -13,15 +13,20 @@ def get_commit_path_input():
     commit_path = input("Enter the path to the commit file (.docx): ").strip()
     return commit_path
 
+def check_commit_path_input():
+    commit_path = get_commit_path_input()
+    if commit_path == "":
+        print("Commit file path cannot be empty.")
+        sys.exit(1)
+
+    if not Path(commit_path).is_file() or Path(commit_path).suffix.lower() != ".docx":
+        print("Invalid commit file path, make sure the file exists and is a .docx file")
+        sys.exit(1)
+    return commit_path
+
+check_commit_path_input()
+
 commit_path = get_commit_path_input()
-
-if commit_path == "":
-    print("Commit file path cannot be empty.")
-    sys.exit(1)
-
-if not Path(commit_path).is_file() or Path(commit_path).suffix.lower() != ".docx":
-    print("Invalid commit file path, make sure the file exists and is a .docx file")
-    sys.exit(1)
 
 confirm = input(f"Are you sure you want to overwrite '{os.path.basename(path)}' with the contents of '{os.path.basename(commit_path)}'?\nThis action will replace the current content of the .docx file. (Y/N): ").strip().lower()
 if confirm != 'y':
