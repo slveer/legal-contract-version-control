31dacb185a918578601835e0cd89636bee0fa5de
Daniel <danielaphillion@gmail.com>
2026-05-23 14:22:02 -0400

    fix: stop relying on global var for func

--- diff.py
+++ diff.py
@@ -155,14 +155,13 @@ def convert_html_to_soup(html):
 
 def format_bs4_html_list(bs4_obj):
     tags_to_list(number_tags(remove_inline_semantics(copy.copy(bs4_obj))))
-
 def get_opcodes(commit_soup, current_soup):
     return difflib.SequenceMatcher(None, tags_to_list(remove_inline_semantics(copy.copy(commit_soup))), tags_to_list(remove_inline_semantics(copy.copy(current_soup)))).get_opcodes()
 
 def get_redline_html(commit_soup):
     return number_tags(remove_inline_semantics(copy.copy(commit_soup)))
 
-def format_redline_html(redline, opcodes):
+def format_redline_html(redline, opcodes, commit_list, docx_current_version_list):
     for opcode in reversed(opcodes):
         tag, i1, i2, j1, j2 = opcode
         
@@ -198,6 +197,6 @@ commit_list = format_bs4_html_list(bs4_commit_soup)
 opcodes = get_opcodes(bs4_commit_soup, bs4_docx_current_version_soup)
 redline = get_redline_html(bs4_commit_soup)
 
-format_redline_html(redline, opcodes)
+format_redline_html(redline, opcodes, commit_list, docx_current_version_list)
 
 write_redline_html_file(redline)
